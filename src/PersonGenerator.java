865a453d0c18f1c18a9fd9984561211f83d0e80e
nate-osterfeld <osterfnl@mail.uc.edu>
2024-09-10 14:34:44 -0400

    Init commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> recs = new ArrayList<>();
+
+        Scanner in = new Scanner(System.in);
+        boolean doneInput = false;
+        String ID = "";
+        String fName = "";
+        String lName = "";
+        String title = "";
+        int yob = 0;
+
+        String rec = ""; // full record of above fields
+
+
+        // uses a fixed known path:
+        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+        // use the toolkit to get the current working directory of the IDE
+        // will create the file within the Netbeans project src folder
+        // (may need to adjust for other IDE)
+        // Not sure if the toolkit is thread safe...
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonData.txt");
+
+        do
+        {
+            ID = SafeInput.getNonZeroLenString(in, "Enter your ID");
+            fName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+            lName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+            title = SafeInput.getNonZeroLenString(in, "Enter title");
+            yob = SafeInput.getRangedInt(in, "Enter year of birth", 1000, 9999);
+
+            rec = ID + ", " + fName + ", " + lName + ", " + title + ", " + yob;
+
+            System.out.println(rec);
+            recs.add(rec);
+
+            doneInput = SafeInput.getYNConfirm(in, "Do you have another record?");
+
+        } while (doneInput);
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String r : recs)
+            {
+                writer.write(r, 0, r.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
